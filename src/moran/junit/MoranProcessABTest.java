949d99f4336559573d0dd93cb55e4ab22a2a6f1d
tipplerow <jsshaff@berkeley.edu>
2019-10-03 09:40:47 -0700

    Refactoring the Moran process tests

--- src/moran/junit/MoranProcessABTest.java
+++ src/moran/junit/MoranProcessABTest.java
@@ -1,47 +1,37 @@
 
 package moran.junit;
 
-import java.util.List;
-
 import jam.bravais.Lattice;
 
-import moran.ab.ABCell;
 import moran.ab.ABConfig;
 import moran.ab.ABType;
 import moran.cell.Cell;
-import moran.driver.MoranProcess;
 import moran.space.Space;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class MoranProcessABTest {
+public class MoranProcessABTest extends MoranProcessTestBase {
     static {
-        System.setProperty(ABConfig.FITNESS_RATIO_PROPERTY, "1.10");
+        System.setProperty(ABConfig.FITNESS_RATIO_PROPERTY, "1.25");
         System.setProperty(ABConfig.MUTATION_RATE_PROPERTY, "0.01");
     }
 
-    private Space createSpace() {
+    private static final double TOLERANCE = 0.0001;
+
+    public MoranProcessABTest() {
+        super(TOLERANCE);
+    }
+
+    @Test public void testRun() {
+        runTest(25, 23.5079, 1.1671, false);
+    }
+
+    @Override protected Space createSpace() {
         Lattice<Cell> lattice = Lattice.parse("HEXAGONAL; 1.0; 100, 100");
         lattice.fill(ABType.A.objectFactory());
 
         return Space.lattice(lattice);
-    }
-
-    private MoranProcess createProcess() {
-        return MoranProcess.initialize(createSpace());
-    }
-
-    @Test public void testRun() {
-        MoranProcess process = createProcess();
-        System.out.println(process.getMeanFitness());
-
-        for (int step = 0; step < 100; ++step) {
-            process.executeTimeStep();
-
-            System.out.println(String.format("STEP: %5d; CLOCK: %10.4f; FITNESS: %6.4f",
-                                             step, process.getTimeClock(), process.getMeanFitness()));
-        }
     }
 
     public static void main(String[] args) {
---
+++ src/moran/junit/MoranProcessTestBase.java
@@ -0,0 +1,66 @@
+
+package moran.junit;
+
+import jam.junit.NumericTestBase;
+
+import moran.driver.MoranProcess;
+import moran.space.Space;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public abstract class MoranProcessTestBase extends NumericTestBase {
+    /**
+     * Creates a new test with a given floating-point tolerance.
+     *
+     * @param tolerance the tolerance to allow in floating-point
+     * equality tests.
+     */
+    protected MoranProcessTestBase(double tolerance) {
+        super(tolerance);
+    }
+
+    /**
+     * Creates the cellular space to simulate.
+     *
+     * @return the cellular space to simulate.
+     */
+    protected abstract Space createSpace();
+
+    /**
+     * Executes the Moran process and tests the results.
+     *
+     * @param stepCount the number of steps to execute.
+     *
+     * @param timeClock the expected value of the continuous time
+     * clock at the end of the simulation.
+     *
+     * @param meanFitness the expected value of the average cell
+     * fitness at the end of the simulation.
+     *
+     * @param verbose whether to log the intermediate steps of the
+     * simulation.
+     */
+    protected void runTest(int     stepCount,
+                           double  timeClock,
+                           double  meanFitness,
+                           boolean verbose) {
+        MoranProcess process =
+            MoranProcess.initialize(createSpace());
+
+        for (int stepIndex = 0; stepIndex < stepCount; ++stepIndex) {
+            process.executeTimeStep();
+
+            if (verbose)
+                logProcess(process, stepIndex);
+        }
+
+        assertDouble(timeClock, process.getTimeClock());
+        assertDouble(meanFitness, process.getMeanFitness());
+    }
+
+    private void logProcess(MoranProcess process, int stepIndex) {
+        System.out.println(String.format("STEP: %5d; CLOCK: %10.4f; FITNESS: %6.4f",
+                                         stepIndex, process.getTimeClock(), process.getMeanFitness()));
+    }
+}
